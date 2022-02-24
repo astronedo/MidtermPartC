dc82c70cefe8ca3f0e46c6a8691222bd615ed1ed
astro <astro@192.168.2.46>
2022-02-23 20:01:06 -0500

    Added getGenres

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+//import java.util.Scanner;
+
+/**
+ *
+ * @author astro
+ */
+public class TestUserprofile {
+    public static void main(String args[]){
+        UserProfile a=new UserProfile();
+        //Scanner input = new Scanner(System.in);
+        String userName="Astronedo";
+        //String userName=input.nextLine();
+        a.setUserID(userName);
+        a.getUserID();
+        a.
+        
+    }
+}
